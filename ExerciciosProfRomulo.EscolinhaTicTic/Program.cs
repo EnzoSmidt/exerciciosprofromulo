b00c5fb6c034a2cf20cb07ba05fa5c7b80111b3c
EnzoSmidt <enzosmidt3000@gmail.com>
2022-07-06 11:53:24 -0300

    exerciciosprofromulo

---
+++ ExerciciosProfRomulo.EscolinhaTicTic/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.EscolinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3;
+            double soma;
+            double media;
+            double somafinal;
+            double mediafinal;
+            string nome;
+            
+
+            //entrada dos dados
+            Console.WriteLine("Qual seu nome Rovem Rovem");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota");             
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota");
+            nota3 = double.Parse(Console.ReadLine());
+
+            soma = nota1 + nota2 + nota3;         
+            media = soma / 3;
+            
+            Console.WriteLine();
+            Console.WriteLine(nome);
+
+            Console.WriteLine();
+            somafinal = Math.Round(soma, 2, MidpointRounding.AwayFromZero);
+            Console.WriteLine($"A soma é:{somafinal}");                     
+
+            Console.WriteLine();
+
+            if (media >= 7)
+            {
+
+                mediafinal = Math.Round(media, 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"O aluno {nome} obteve a média {mediafinal} e foi aprovado.");            
+                Console.WriteLine();            
+            }
+            else
+            {
+                mediafinal = Math.Round(media, 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"O aluno {nome} obteve a média {mediafinal} e foi reprovado.");
+                Console.WriteLine();
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExerciciosProfRomulo.MatematicaBasica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.MatematicaBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercício 3- No projeto MatematicaBasica crie um 
+               Programa que calcule a soma de dois números inteiros.
+               Considere que o usuário vai digitar esses dois números.
+            */
+            //declarar as variáveis
+            int num1;
+            int num2;
+            int soma;
+            //entrada de dados
+            Console.WriteLine("Jovem Jovem digite o primeiro número");
+            num1 = int.Parse(Console.ReadLine());       
+
+            Console.WriteLine("Jovem Jovem digite o segundo número");
+            num2 = int.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+            Console.WriteLine(soma);
+
+            Console.ReadKey();
+        }
+    }
+}

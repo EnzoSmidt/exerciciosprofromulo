150eae94d77711c6735d9dc620794e4376dc4443
EnzoSmidt <enzosmidt3000@gmail.com>
2022-07-07 08:13:00 -0300

    salvandoatividade6

---
+++ ExerciciosProfRomulo.Atividade6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.Atividade6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int numPar = 0;
+
+            do
+            {
+                i = i + 2;
+                numPar = i;
+                Console.WriteLine(numPar);
+            }
+            while (i < 200);
+
+            Console.ReadKey();
+        }
+    }
+}
